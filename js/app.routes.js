fab68624b4585f8634d7189c6d66d6edeb78c9d2
Andrei Oprea <andrei.br92@gmail.com>
2014-03-18 23:34:04 +0200

    add all scripts locally

--- js/app.routes.js
+++ js/app.routes.js
@@ -1,39 +1,38 @@
 /* globals Backbone */
 
 (function() {
-'use strict';
+    'use strict';
 
-window.App = window.App || {};
-window.App.Routes = Backbone.Router.extend({
+    window.App = window.App || {};
+    window.App.Routes = Backbone.Router.extend({
 
- routes: {
-    ''        : 'index',
-    'wallet'  : 'wallet',
-    'currency': 'currency',
-    'graph'   : 'graph'
-  },
+        routes: {
+            ''        : 'index',
+        'wallet'  : 'wallet',
+        'currency': 'currency',
+        'graph'   : 'graph'
+        },
 
-  index: function() {
-  },
+        index: function() {
+        },
 
-  wallet: function () {
-    $('#expense-wallet').toggleClass('hidden');
-    $('#expense-settings').toggleClass('hidden');
-  },
+        wallet: function () {
+            $('#expense-wallet').toggleClass('hidden');
+            $('#expense-settings').toggleClass('hidden');
+        },
 
-  currency: function () {
-    $('#expense-wallet').toggleClass('hidden');
-    $('#expense-settings').toggleClass('hidden');
-  },
+        currency: function () {
+            $('#expense-wallet').toggleClass('hidden');
+            $('#expense-settings').toggleClass('hidden');
+        },
 
-  graph: function() {
-    $('#expense-wallet').hide();
-    $('#expense-settings').hide();
-    $('#expense-graph').removeClass('hidden');
-    plotChart();
-  }
+        graph: function() {
+            $('#expense-wallet').hide();
+            $('#expense-settings').hide();
+            $('#expense-graph').removeClass('hidden');
+        }
 
 
-});
+    });
 
 })();
